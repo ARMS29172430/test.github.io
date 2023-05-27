0b62533ff18f097c3c0ca41aa1aec56c462d3411
B R Sathwik <brsathwik@gmail.com>
2023-05-28 03:40:57 +0530

    added react-router-dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Management from './screens/management/management';
 import Navbar from './layouts/navbar/navbar.component';
 import Footer from './layouts/footer/footer.component';
 import ContactUs from './screens/contact-us/contact-us';
+import './App.scss';
+
+// create a routes array to manage all routes
+const routes: { path: string; element: JSX.Element }[] = [
+  { path: '', element: <div>Home!</div> },
+  { path: 'about-us', element: <div>About us!</div> },
+  { path: 'contact-us', element: <ContactUs /> },
+  { path: 'management', element: <Management /> },
+];
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Management />
-      <ContactUs />
+      <Routes>
+        {routes.map((routeObj) => (
+          <Route {...routeObj} />
+        ))}
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
